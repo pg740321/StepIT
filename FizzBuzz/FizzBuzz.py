207ca498e4944f1b7b71b8d091832905fb9877e2
pg740321 <pg740321@gmail.com>
2023-11-09 23:08:33 +0100

    format

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -1,22 +1,20 @@
 cis = input('Zadej cislo 1 .. 100 :');
 
-if not cis.isnumeric() :
-  print('Zadany udaj neni cislo !!!!!');
-  exit();
-
+if not cis.isnumeric():
+    print('Zadany udaj neni cislo !!!!!');
+    exit();
 
 cislo = int(cis);
 
-if (cislo<1) or (cislo>100):
-  print('Zadane cislo neni v rozsahu 1 az 100');
-  exit();
-
+if (cislo < 1) or (cislo > 100):
+    print('Zadane cislo neni v rozsahu 1 az 100');
+    exit();
 
 # Prvni moznost reseni
 
 slovo = '';
 
-if (cislo % 3 == 0) :
+if (cislo % 3 == 0):
     slovo = slovo + 'Fizz';
 
 if (cislo % 5 == 0):
@@ -30,15 +28,13 @@ print(slovo);
 # ------------------------------------------------------------------------
 
 
-
 # druha moznost reseni
 
 if (cislo % 3 == 0) and (cislo % 5 == 0):
-  print('FizzBuzz')
+    print('FizzBuzz')
 elif (cislo % 3 == 0):
-  print('Fizz')
-elif  (cislo % 5 == 0):
-  print('Buzz')
-else :
-  print(cislo);
-
+    print('Fizz')
+elif (cislo % 5 == 0):
+    print('Buzz')
+else:
+    print(cislo);
