f2088b93c153682791b726bf2b3cfed612579750
gratzerp <pavel.gratzer@ruk.cuni.cz>
2024-04-03 18:23:27 +0200

    python

---
+++ obrazce.py
@@ -0,0 +1,30 @@
+import turtle as tur
+
+import colorsys as cs
+
+tur.setup(800,800)
+tur.hideturtle()
+tur.speed(0)
+tur.tracer(0)
+tur.width(1)
+tur.bgcolor("black")
+
+for j in range(25):
+    for i in range(15):
+        tur.begin_fill()
+
+        tur.color(cs.hsv_to_rgb(i/15,j/25,1))
+
+        tur.right(90)
+
+        tur.circle(200-j*4,90)
+        tur.left(90)
+
+        tur.circle(200-j*4,90)
+        tur.right(180)
+
+        tur.circle(10,24)
+
+        tur.end_fill()
+
+tur.done()
